b1db3b560ef7441d08568e3fd1221c9d2834d3fd
Mikhailo Bidyak <sgame666@gmail.com>
2018-09-10 21:07:37 +0300

    Added unittest for Calc.add

---
+++ test_calc.py
@@ -0,0 +1,11 @@
+import unittest
+import Calc
+
+class TestCalc(unittest.TestCase):
+    def test_add(self):
+        self.assertEqual(Calc.add(10, 5), 15)
+        self.assertEqual(Calc.add(-1, 1), 0)
+        self.assertEqual(Calc.add(-1, -1), -2)
+
+if __name__ == '__main__':
+    unittest.main()
