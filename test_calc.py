46b3bd2de394960b263960d967d911f14637260e
Mikhailo Bidyak <sgame666@gmail.com>
2018-09-10 21:37:16 +0300

    Added unittest for subtract, multiply, divide

--- test_calc.py
+++ test_calc.py
@@ -1,11 +1,30 @@
 import unittest
 import Calc
 
-class TestCalc(unittest.TestCase):
+class Test(unittest.TestCase):
     def test_add(self):
         self.assertEqual(Calc.add(10, 5), 15)
         self.assertEqual(Calc.add(-1, 1), 0)
         self.assertEqual(Calc.add(-1, -1), -2)
+
+    def test_subtract(self):
+        self.assertEqual(Calc.subtract(10, 5), 5)
+        self.assertEqual(Calc.subtract(-1, 1), -2)
+        self.assertEqual(Calc.subtract(-1, -1), 0)
+
+    def test_multiply(self):
+        self.assertEqual(Calc.multiply(10, 5), 50)
+        self.assertEqual(Calc.multiply(-1, 1), -1)
+        self.assertEqual(Calc.multiply(-1, -1), 1)
+
+    def test_divide(self):
+        self.assertEqual(Calc.divide(10, 5), 2)
+        self.assertEqual(Calc.divide(-1, 1), -1)
+        self.assertEqual(Calc.divide(-1, -1), 1)
+        self.assertEqual(Calc.divide(5, 2), 2.5)
+
+        with self.assertRaises(ValueError):
+            Calc.divide(10, 0)
 
 if __name__ == '__main__':
     unittest.main()
